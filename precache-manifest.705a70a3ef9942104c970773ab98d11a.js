0acccd0443771d4e7c3611c3c5d90c7308607724
mnml <rjl.macfarlane@gmail.com>
2022-04-25 16:30:16 -0300

    Update

---
+++ precache-manifest.705a70a3ef9942104c970773ab98d11a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "80c8ee717a7d13dfd4f0",
+    "url": "./static/css/main.7902f80d.chunk.css"
+  },
+  {
+    "revision": "80c8ee717a7d13dfd4f0",
+    "url": "./static/js/main.80c8ee71.chunk.js"
+  },
+  {
+    "revision": "90320a4596191f6bbc0d",
+    "url": "./static/js/1.90320a45.chunk.js"
+  },
+  {
+    "revision": "0d176349c7a613dbc327",
+    "url": "./static/js/2.0d176349.chunk.js"
+  },
+  {
+    "revision": "4db01c3506a2ad5b171b",
+    "url": "./static/js/runtime~main.4db01c35.js"
+  },
+  {
+    "revision": "dfe5fb65bd8dc05a23a7d320c7a1a030",
+    "url": "./index.html"
+  }
+];
